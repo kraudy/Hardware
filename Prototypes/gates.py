d864a71e33583fdf308850cab7f2938378b777b4
Roberto Torrez <robkraudy@gmail.com>
2026-01-19 21:47:03 -0600

    Add nand gate
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- Prototypes/gates.py
+++ Prototypes/gates.py
@@ -2,7 +2,14 @@
 """
 Simple and gate
 """
-class ANDgate:
+class AndGate:
   def __call__(self, a: bool, b: bool) -> bool:
     return (a & b)
 
+class NandGate:
+  def __call__(self, a: bool, b: bool) -> bool:
+    return not (a & b)
+  
+class OrGate:
+  def __call__(self, a:bool, b: bool) -> bool:
+    return (a | b)
--- Prototypes/test_gates.py
+++ Prototypes/test_gates.py
@@ -1,7 +1,7 @@
-from gates import ANDgate
+from gates import AndGate, NandGate, OrGate
 
-def test_ANDgate():
-  and1 = ANDgate()
+def test_AndGate():
+  and1 = AndGate()
 
   truth_table = [
     (0, 0, 0),
@@ -13,8 +13,39 @@ def test_ANDgate():
   for a, b, y in truth_table:
     assert and1(a, b) == y, f"Failed: {a} AND {b} -> {y}"
   
-  print("test ANDgate successful")
+  print("test AndGate successful")
 
+def test_NandGate():
+  Nand1 = NandGate()
+
+  truth_table = [
+    (0, 0, 1),
+    (0, 1, 1),
+    (1, 0, 1),
+    (1, 1, 0)
+  ]
+
+  for a, b, y in truth_table:
+    assert Nand1(a, b) == y, f"Failed: {a} AND {b} -> {y}"
+  
+  print("test NandGate successful")
+
+def test_OrGate():
+  or1 = OrGate()
+
+  truth_table = [
+    (0, 0, 0),
+    (0, 1, 1),
+    (1, 0, 1),
+    (1, 1, 1)
+  ]
+
+  for a, b, y in truth_table:
+    assert or1(a, b) == y, f"Failed: {a} AND {b} -> {y}"
+  
+  print("test OrGate successful")
 
 if __name__ == "__main__":
-  test_ANDgate()
+  test_AndGate()
+  test_NandGate()
+  test_OrGate()
